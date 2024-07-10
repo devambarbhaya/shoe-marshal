fc173826bc83b53a6671a7f5f5a30e45a4993bb8
Devam Barbahya <barbhaya.devam@gmail.com>
2024-07-10 10:14:05 +0530

    Create Dashboard Products Page

---
+++ app/dashboard/products/page.tsx
@@ -0,0 +1,89 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { MoreHorizontal, PlusCircle, UserIcon } from "lucide-react";
+import Link from "next/link";
+
+export default function ProductsPage() {
+  return (
+    <>
+      <div className="flex items-center justify-end">
+        <Button asChild className="flex items-center gap-x-2">
+          <Link href="/dashboard/products/create">
+            <PlusCircle className="h-5 w-5" />
+            <span>Add Product</span>
+          </Link>
+        </Button>
+      </div>
+      <Card className="mt-5">
+        <CardHeader>
+          <CardTitle>Products</CardTitle>
+          <CardDescription>
+            Manage Your Products and View their Sales Performance
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Image</TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead>Price</TableHead>
+                <TableHead>Date</TableHead>
+                <TableHead className="text-right">Actions</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <UserIcon className="h-16 w-16" />
+                </TableCell>
+                <TableCell>Nike Air</TableCell>
+                <TableCell>Active</TableCell>
+                <TableCell>$149.00</TableCell>
+                <TableCell>09-07-2024</TableCell>
+                <TableCell className="text-right">
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button variant="ghost" size="icon">
+                        <MoreHorizontal className="h-4 w-4" />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                      <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem>Edit</DropdownMenuItem>
+                      <DropdownMenuItem>Delete</DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+    </>
+  );
+}
--- components/dashboard/DashboardNavigation.tsx
+++ components/dashboard/DashboardNavigation.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const links = [
   {
@@ -20,10 +24,19 @@ const links = [
 ];
 
 export function DashboardNavigation() {
+  const pathname = usePathname();
   return (
     <>
       {links.map((link) => (
-        <Link key={link.href} href={link.href}>
+        <Link
+          key={link.href}
+          href={link.href}
+          className={cn(
+            link.href === pathname
+              ? "text-foreground"
+              : "text-muted-foreground hover:text-foreground"
+          )}
+        >
           {link.name}
         </Link>
       ))}
